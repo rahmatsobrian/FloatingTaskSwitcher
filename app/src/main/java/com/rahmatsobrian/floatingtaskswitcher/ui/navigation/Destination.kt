427621446a32775a39871cd16d1aad5b05a3ea3a
Rahmat Sobrian <135594470+rahmatsobrian@users.noreply.github.com>
2026-08-06 09:00:40 +0700

    Delete folder app

--- app/src/main/java/com/rahmatsobrian/floatingtaskswitcher/ui/navigation/Destination.kt
+++
@@ -1,7 +0,0 @@
-package com.rahmatsobrian.floatingtaskswitcher.ui.navigation
-
-sealed class Destination(val route: String) {
-    data object Home : Destination("home")
-    data object PermissionManager : Destination("permission_manager")
-    data object Settings : Destination("settings")
-}
